368fd4da32f3083f63fff555c1dcd9c754e46a7f
Rehan-24 <rikhan6855@gmail.com>
2023-06-18 12:44:39 -0700

    refactored front end and back end to follow very basic template format

--- red_sent/src/App.js
+++ red_sent/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [submitting, setSubmitting] = useState(false);
+  /*const [submitting, setSubmitting] = useState(false);
   
   const handleSubmit = event => {
     event.preventDefault();
@@ -16,12 +16,35 @@ function App() {
   function upload(redditTopic) {
     alert(redditTopic);
   }
+*/
+// hooks for using states without creating classes
+// const [cont, setCount] using setCount, we can update count
+// data fetching would require effect hooks, i think
+const [articles, setArticles] = useState([])
 
+useEffect(() => {
+  // local host for now
+  fetch('http://localhost:3000/get', {
+    'method': 'GET', 
+    headers:{
+      'Content-Type': 'applications/json'
+    }
+  })
+  .then(resp => resp.json())
+  .then(resp => console.log(resp))
+  .catch(error => console.log(error0))
+
+
+}, [])
 
   
 
   return(
-    <div className="wrapper">
+    <div className="App">
+      <h1>Flask and React JS template by RK24</h1>
+    </div>
+   /*
+   <div className="wrapper">
       <h1>Reddit Sentiment Tracker</h1>
       {submitting &&
        <div>Submtting Form...</div>
@@ -42,6 +65,7 @@ function App() {
     </button>
       </form>
     </div>
+  */
   )
 }
 
